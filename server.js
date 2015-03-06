f163266525ec2bf39fc83ba76a9903e9f1ac7d5d
Bogdan Muresan <bogdan.muresan@freshbyteinc.com>
2015-03-07 00:58:45 +0200

    Create net socket server

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 // Require dependencies
 var app = require('http').createServer(handler)
 , fs = require('fs')
-, io = require('socket.io').listen(app);
- 
+, io = require('socket.io').listen(app)
+, net = require('net');
+
+var WS_PORT = 6555;
+var NET_PORT = 7555;
+
+var server_data;
+
 // creating the server ( localhost:8000 )
-app.listen(6555);
+app.listen(WS_PORT);
 
 // on server started we can load our client.html page
 function handler(req, res) {
@@ -18,7 +24,20 @@ function handler(req, res) {
     res.end(data);
   });
 }
- 
+
+
+// Get server client data
+net.createServer(function(sock) {
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+        server_data = data;
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+
+    });
+}).listen(NET_PORT);
+
+
+
 // creating a new websocket to keep the content updated without any AJAX request
 io.sockets.on('connection', function(socket) {
 
