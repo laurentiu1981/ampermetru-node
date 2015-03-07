597c4e82afef16b51adb5a99c5a544b4f675f718
Laurentiu Lese [Freshbyte] <laurentiu.lese@freshbyteinc.com>
2015-03-07 05:46:45 +0200

    Update hosts for client.

--- server.js
+++ server.js
@@ -98,6 +98,7 @@ net.createServer(function(sock) {
 // creating a new websocket to keep the content updated without any AJAX request
 io.sockets.on('connection', function (socket) {
 
+  io.sockets.volatile.emit('send hosts', hosts);
   socket.on('display change', function (data) {
     displayType = data;
     io.sockets.volatile.emit('changed display', data);
